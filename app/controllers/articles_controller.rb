4004697ee0abce31503e1e5837485ce018647202
Denzyl Balram <denzyl.balram@gmail.com>
2015-12-16 16:59:32 -0800

    Implement show article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,16 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash.now[:success] = "Article has been created"
+      flash[:success] = "Article has been created"
       redirect_to articles_path
     else
       flash.now[:danger] = "Article has not been created"
       render :new
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
